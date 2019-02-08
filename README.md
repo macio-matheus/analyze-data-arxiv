c4b1a04d3c93344c3d383ee12c85f437c504aa37
Mácio Matheus Arruda <macio.matheus.arruda@accenture.com>
2019-02-08 16:53:12 -0300

    Clean repo with final version

--- README.md
+++ README.md
@@ -1,12 +1,17 @@
 # analyze-data-arxiv
 Data analysis of the website http://arxiv.org
 
-```sh
-# Start the jupyter using docker
-docker-compose up -d
-```
+Chosen topic: math
 
-Folder structure
+### Team
+
+- Henrique Lima (https://github.com/henriquelima1408)
+- Julio Sales (https://github.com/jsalesba)
+- Mácio Matheus Arruda (https://github.com/macio-matheus)
+- Victor Outtes (https://github.com/victorouttes)
+
+
+### Folder structure
 
 ```sh
     * notebooks - Contains all notebooks
@@ -14,16 +19,43 @@ Folder structure
     * models - Must contains all machine learning models
 ```
 
-Dataset
+### Dataset
 
 ```sh
-   wget https://www.dropbox.com/s/s5mysx0b6sp7jvw/authors.json -P ../datasets/
-   wget https://www.dropbox.com/s/ktvn3q2ijfka9zn/authors_paper.json -P ../datasets/
-   wget https://www.dropbox.com/s/jbqvl5htdv41c33/papers.json -P ../datasets/
+   wget https://www.dropbox.com/s/k6knmy7pvqiqo8v/_author_paper.json -P ../datasets/
+   wget https://www.dropbox.com/s/z5qz0al2eezdlin/_papers.json -P ../datasets/
 ```
 
+### Dataframe paper
 
-Clustering formed by feature extraction of abstracts from papers
+![clusters](https://raw.githubusercontent.com/macio-matheus/analyze-data-arxiv/master/docs/dataframe-paper.png)
 
-![clusters](https://raw.githubusercontent.com/macio-matheus/analyze-data-arxiv/master/docs/clusters.png)
- 
+
+### Network year one
+
+![clusters](https://raw.githubusercontent.com/macio-matheus/analyze-data-arxiv/master/docs/network-preview.png)
+
+### Usage
+First of all, build the container using docker-compose and then you can 
+access the Jupyter that is ready to be used.
+
+#### Run with docker compose
+```sh
+cd analyse-data-arxiv
+docker-compose up -d
+```
+
+#### Accessing Jupyter
+```sh
+http://<your-ip>:8899/tree
+```
+
+#### Ports
+```sh
+- 8899 => Jupyter
+```
+
+### DockerHub
+```sh
+https://hub.docker.com/r/maciomatheus/jupyter_notebook_data_science/
+```
--- notebooks/arxivscraper.py
+++ notebooks/arxivscraper.py
@@ -9,6 +9,7 @@ import xml.etree.ElementTree as ET
 import datetime
 import time
 import sys
+
 PYTHON3 = sys.version_info[0] == 3
 if PYTHON3:
     from urllib.parse import urlencode
@@ -20,6 +21,7 @@ else:
 OAI = '{http://www.openarchives.org/OAI/2.0/}'
 ARXIV = '{http://arxiv.org/OAI/arXiv/}'
 BASE = 'http://export.arxiv.org/oai2?verb=ListRecords&'
+
 
 class Record(object):
     """
@@ -57,14 +59,14 @@ class Record(object):
 
     def output(self):
         d = {'title': self.title,
-         'id': self.id,
-         'abstract': self.abstract,
-         'categories': self.cats,
-         'doi': self.doi,
-         'created': self.created,
-         'updated': self.updated,
-         'authors': self.authors,
-         'url': self.url}
+             'id': self.id,
+             'abstract': self.abstract,
+             'categories': self.cats,
+             'doi': self.doi,
+             'created': self.created,
+             'updated': self.updated,
+             'authors': self.authors,
+             'url': self.url}
         return d
 
 
@@ -96,7 +98,7 @@ class Scraper(object):
     Returning all eprints from
     """
 
-    def __init__(self, category, date_from=None, date_until=None, t=5, filters={}):
+    def __init__(self, category, date_from=None, date_until=None, t=15, filters={}):
         self.cat = str(category)
         self.t = t
         DateToday = datetime.date.today()
@@ -164,7 +166,7 @@ class Scraper(object):
 
         t1 = time.time()
         print('fetching is completed in {0:.1f} seconds.'.format(t1 - t0))
-        print ('Total number of records {:d}'.format(len(ds)))
+        print('Total number of records {:d}'.format(len(ds)))
         return ds
 
 
@@ -177,35 +179,36 @@ def search_all(df, col, *words):
 
 
 cats = [
- 'astro-ph', 'cond-mat', 'gr-qc', 'hep-ex', 'hep-lat', 'hep-ph', 'hep-th',
- 'math-ph', 'nlin', 'nucl-ex', 'nucl-th', 'physics', 'quant-ph', 'math', 'CoRR', 'q-bio',
- 'q-fin', 'stat']
+    'astro-ph', 'cond-mat', 'gr-qc', 'hep-ex', 'hep-lat', 'hep-ph', 'hep-th',
+    'math-ph', 'nlin', 'nucl-ex', 'nucl-th', 'physics', 'quant-ph', 'math', 'CoRR', 'q-bio',
+    'q-fin', 'stat']
 subcats = {'cond-mat': ['cond-mat.dis-nn', 'cond-mat.mtrl-sci', 'cond-mat.mes-hall',
-              'cond-mat.other', 'cond-mat.quant-gas', 'cond-mat.soft', 'cond-mat.stat-mech',
-              'cond-mat.str-el', 'cond-mat.supr-con'],
- 'hep-th': [],'hep-ex': [],'hep-ph': [],
- 'gr-qc': [],'quant-ph': [],'q-fin': ['q-fin.CP', 'q-fin.EC', 'q-fin.GN',
-           'q-fin.MF', 'q-fin.PM', 'q-fin.PR', 'q-fin.RM', 'q-fin.ST', 'q-fin.TR'],
+                        'cond-mat.other', 'cond-mat.quant-gas', 'cond-mat.soft', 'cond-mat.stat-mech',
+                        'cond-mat.str-el', 'cond-mat.supr-con'],
+           'hep-th': [], 'hep-ex': [], 'hep-ph': [],
+           'gr-qc': [], 'quant-ph': [], 'q-fin': ['q-fin.CP', 'q-fin.EC', 'q-fin.GN',
+                                                  'q-fin.MF', 'q-fin.PM', 'q-fin.PR', 'q-fin.RM', 'q-fin.ST',
+                                                  'q-fin.TR'],
 
- 'nucl-ex': [],'CoRR': [],'nlin': ['nlin.AO', 'nlin.CG', 'nlin.CD', 'nlin.SI',
-          'nlin.PS'],
- 'physics': ['physics.acc-ph', 'physics.app-ph', 'physics.ao-ph',
-             'physics.atom-ph', 'physics.atm-clus', 'physics.bio-ph', 'physics.chem-ph',
-             'physics.class-ph', 'physics.comp-ph', 'physics.data-an', 'physics.flu-dyn',
-             'physics.gen-ph', 'physics.geo-ph', 'physics.hist-ph', 'physics.ins-det',
-             'physics.med-ph', 'physics.optics', 'physics.ed-ph', 'physics.soc-ph',
-             'physics.plasm-ph', 'physics.pop-ph', 'physics.space-ph'],
- 'math-ph': [],
- 'math': ['math.AG', 'math.AT', 'math.AP', 'math.CT', 'math.CA', 'math.CO',
-          'math.AC', 'math.CV', 'math.DG', 'math.DS', 'math.FA', 'math.GM', 'math.GN',
-          'math.GT', 'math.GR', 'math.HO', 'math.IT', 'math.KT', 'math.LO', 'math.MP',
-          'math.MG', 'math.NT', 'math.NA', 'math.OA', 'math.OC', 'math.PR', 'math.QA',
-          'math.RT', 'math.RA', 'math.SP', 'math.ST', 'math.SG'],
- 'q-bio': ['q-bio.BM',
-           'q-bio.CB', 'q-bio.GN', 'q-bio.MN', 'q-bio.NC', 'q-bio.OT', 'q-bio.PE', 'q-bio.QM',
-           'q-bio.SC', 'q-bio.TO'],
- 'nucl-th': [],'stat': ['stat.AP', 'stat.CO', 'stat.ML',
-          'stat.ME', 'stat.OT', 'stat.TH'],
- 'hep-lat': [],'astro-ph': ['astro-ph.GA',
-              'astro-ph.CO', 'astro-ph.EP', 'astro-ph.HE', 'astro-ph.IM', 'astro-ph.SR']
- }
+           'nucl-ex': [], 'CoRR': [], 'nlin': ['nlin.AO', 'nlin.CG', 'nlin.CD', 'nlin.SI',
+                                               'nlin.PS'],
+           'physics': ['physics.acc-ph', 'physics.app-ph', 'physics.ao-ph',
+                       'physics.atom-ph', 'physics.atm-clus', 'physics.bio-ph', 'physics.chem-ph',
+                       'physics.class-ph', 'physics.comp-ph', 'physics.data-an', 'physics.flu-dyn',
+                       'physics.gen-ph', 'physics.geo-ph', 'physics.hist-ph', 'physics.ins-det',
+                       'physics.med-ph', 'physics.optics', 'physics.ed-ph', 'physics.soc-ph',
+                       'physics.plasm-ph', 'physics.pop-ph', 'physics.space-ph'],
+           'math-ph': [],
+           'math': ['math.AG', 'math.AT', 'math.AP', 'math.CT', 'math.CA', 'math.CO',
+                    'math.AC', 'math.CV', 'math.DG', 'math.DS', 'math.FA', 'math.GM', 'math.GN',
+                    'math.GT', 'math.GR', 'math.HO', 'math.IT', 'math.KT', 'math.LO', 'math.MP',
+                    'math.MG', 'math.NT', 'math.NA', 'math.OA', 'math.OC', 'math.PR', 'math.QA',
+                    'math.RT', 'math.RA', 'math.SP', 'math.ST', 'math.SG'],
+           'q-bio': ['q-bio.BM',
+                     'q-bio.CB', 'q-bio.GN', 'q-bio.MN', 'q-bio.NC', 'q-bio.OT', 'q-bio.PE', 'q-bio.QM',
+                     'q-bio.SC', 'q-bio.TO'],
+           'nucl-th': [], 'stat': ['stat.AP', 'stat.CO', 'stat.ML',
+                                   'stat.ME', 'stat.OT', 'stat.TH'],
+           'hep-lat': [], 'astro-ph': ['astro-ph.GA',
+                                       'astro-ph.CO', 'astro-ph.EP', 'astro-ph.HE', 'astro-ph.IM', 'astro-ph.SR']
+           }
